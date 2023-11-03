73b3633d6c6a32ce467abd44d6347c49ba73d0c3
Alvin Cayo <cayoalvin991@gmail.com>
2023-11-02 22:34:35 -0400

    creacion de atomos

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Name } from './components/name'
+import { Paragraph } from './components/paragraph'
+import { Profile } from './components/profile'
+import { Button } from './components/button'
+import { Button2 } from './components/button2'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Profile></Profile>
+        <Name></Name>
+        <Paragraph></Paragraph>
+        <Button></Button>
+        <Button2></Button2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Profile = () => {
+  return (
+    <div>
+        <img src="./public/Ellipse 1.png" alt="perfil" />
+    </div>
+  )
+}
